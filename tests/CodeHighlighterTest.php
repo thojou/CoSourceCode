992f2d3729e8db327cdf5204aff0a69fb3552dab
Thomas Joußen <tjoussen@databay.de>
2023-09-29 17:41:41 +0200

    Fix Version support

--- tests/CodeHighlighterTest.php
+++ tests/CodeHighlighterTest.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the CoSourceCode Plugin for ILIAS.
+ *
+ * (c) Thomas Joußen <tjoussen@databay.de>
+ *
+ * This source file is subject to the GPL-3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace CoSourceCode\Tests;
 
 use CoSourceCode\CodeHighlighter;
@@ -7,10 +18,11 @@ use PHPUnit\Framework\TestCase;
 
 class CodeHighlighterTest extends TestCase
 {
-
-    public function testInstance()
+    public function testInstance(): void
     {
         $highlighter = new CodeHighlighter();
+
+        $this->assertInstanceOf(CodeHighlighter::class, $highlighter);
     }
 
 }
